71f723c56a780a086b772c752360adec036283cc
Igor Brandão <igorabrandao@gmail.com>
2015-09-24 13:25:22 -0300

    Clean up project assets and created makefile

--- makefile
+++ makefile
@@ -1,49 +1,28 @@
 #
-<<<<<<< HEAD
 # Makefile build 'Game of Life' using Make
-=======
-# Makefile build 'game of life' using Make
->>>>>>> a26909338bb9586f56a7907b3c8a31bccd2f7670
 #
 
 SOURCES = $(wildcard src/*.cpp)
 
 CC      = g++
-<<<<<<< HEAD
 CFLAGS  = -c -g -Wall -std=c++11
-=======
-CFLAGS  = -c -g -Wall -std=c++0x
->>>>>>> a26909338bb9586f56a7907b3c8a31bccd2f7670
 
 INC_DIR = ./include
 BIN_DIR = ./bin
 APP     = $(BIN_DIR)/life
-<<<<<<< HEAD
 LIB_DIR = 
 LIBS = \
 	-lsfml-graphics         \
 	-lsfml-audio 	        \
 	-lsfml-window           \
 	-lsfml-system
-=======
->>>>>>> a26909338bb9586f56a7907b3c8a31bccd2f7670
 
 OBJECTS = $(SOURCES:.cpp=.o)
 
 all: $(SOURCES) $(APP)
     
 $(APP): $(OBJECTS) 
-<<<<<<< HEAD
 	$(CC) -o $@ $(OBJECTS) $(LIBS)
 
 .cpp.o:
 	$(CC) $< -o $@ $(CFLAGS) -I$(INC_DIR)
-=======
-	$(CC) -o $@ $(OBJECTS)
-
-.cpp.o:
-	$(CC) $< -o $@ $(CFLAGS) -I$(INC_DIR)
-
-exe:
-	$(APP)
->>>>>>> a26909338bb9586f56a7907b3c8a31bccd2f7670
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /********************************************//**
 * \drive.cpp
 * \Main
@@ -67,42 +66,3 @@ int main()
 	}
 	return 0;
 }
-=======
-#include <iostream>
-#include <vector>
-#include "life.hpp"
-
-using namespace std;
-
-//! Main (receive the fileName argument from command line)
-/*! The program starts here!
-*/
-int main( int argc, char* argv[] )
-{
-	//! Check the arguments */
-	if ( argc < 2 )
-	{
-		// Inform how to use the program
-		cout << endl << "****************************************************************" << endl;
-        cout << endl << "Use: life <input_cfg_file> [<output_cfg_evolution_file>]" << endl;
-        cout << endl << "****************************************************************" << endl << endl;
-
-        //! Program's earlier end */
-        return 0;
-    }
-    else
-    {
-		//! Life class instance */
-	    Life life;
-
-	    //! Set the file name */
-	    life.setFileName(argv[1]);
-
-	    //! Start the simulation */
-	    life.run();
-    }
-
-    //! Program's end */
-    return 0;
-}
->>>>>>> a26909338bb9586f56a7907b3c8a31bccd2f7670
